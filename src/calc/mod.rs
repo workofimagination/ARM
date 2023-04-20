3e05baa726899abc8a76e8f704d8a7146e181cb3
asphergers <carter44354@gmail.com>
2023-04-20 07:11:34 -0400

    finally fixed the get_angles command for the last time

--- src/calc/mod.rs
+++ src/calc/mod.rs
@@ -1,47 +1,65 @@
+use std::f32::consts::PI;
 
 pub struct Point {
-    pub x: f64,
-    pub y: f64
+    pub x: f32,
+    pub y: f32
 }
 
 pub struct Calc {
     pub origin: Point,
-    pub radius: f64
+    pub radius: f32
 }
 
 impl Calc {
-    pub fn dist(start: &Point, e: &Point) -> f64 {
-        let d = f64::sqrt((start.x - e.x).powi(2) + (start.y - e.y).powi(2));
+    pub fn dist(start: &Point, e: &Point) -> f32 {
+        let d = f32::sqrt((start.x - e.x).powi(2) + (start.y - e.y).powi(2));
 
         return d;
     }
 
-    pub fn get_angles(&self, e: &Point) -> (f64, f64) {
+    pub fn get_angles(&self, e: &Point) -> (f32, f32) {
         let change_x = e.x - self.origin.x;
         let chang_y = e.y - self.origin.y;
 
         let d = Calc::dist(&self.origin, e);
         let a = d/2.0;
-        let h = f64::sqrt(self.radius.powi(2) - a.powi(2));
-        let i = f64::atan(chang_y/change_x);
+        let h = f32::sqrt(self.radius.powi(2) - a.powi(2));
+        let i = f32::atan(chang_y/change_x);
 
-        let theta_one = f64::atan(h/a) + i;
-        
+        let theta_one = f32::atan(h/a) + i;
+
         let point_one = Calc::get_point(theta_one, &self.origin);
-        let change_x2 = e.x - point_one.x;
-        let change_y2 = e.y - point_one.y;
+        let change_x2 = e.x-point_one.x;
+        let change_y2 = e.y-point_one.y;
+        let is_third = i32::abs(i32::signum(f32::signum(change_x2) as i32 + f32::signum(change_y2) as i32 + 2)-1);
 
-        let theta_two = f64::atan(change_y2/change_x2);
+        let theta_two = f32::atan(change_y2/change_x2) + (PI*is_third as f32);
 
-        return (theta_one, theta_two);
+        return (Calc::format_angles(theta_one), Calc::format_angles(theta_two));
     }
 
-    pub fn get_point(angle: f64, center: &Point) -> Point {
-        let x = f64::cos(angle) + center.x;
-        let y = f64::sin(angle) + center.y;
+    pub fn format_angles(angle: f32) -> f32 {
+        if angle > PI {
+            return angle-(2.0*PI);
+        }
+
+        return angle;
+    }
+
+    pub fn to_degree(angle: f32) -> f32 {
+        return (180.0*angle) / PI;
+    }
+
+    pub fn to_radian(angle: f32) -> f32 {
+        return (PI*angle) / (180.0);
+    }
+
+    pub fn get_point(angle: f32, center: &Point) -> Point {
+        let x = f32::cos(angle) + center.x;
+        let y = f32::sin(angle) + center.y;
 
         return Point{ x, y }
-    }
+     }
 
     pub fn test() {
         let origin = Point {
@@ -50,8 +68,8 @@ impl Calc {
         };
 
         let goto = Point {
-            x: 1.0,
-            y: 0.8
+            x: 0.438,
+            y: -0.656
         };
 
         let calc = Calc {
@@ -70,7 +88,9 @@ impl Calc {
         let point_t = Calc::get_point(theta_two, &point_o);
 
         println!("Expected point: {}, {}", goto.x, goto.y);
+        println!("Column angle: {}", theta_one);
         println!("Beam: {}, {}", point_o.x, point_o.y);
+        println!("Beam angle: {}", theta_two);
         println!("Beam tip: {}, {}", point_t.x, point_t.y);
     }
 }
