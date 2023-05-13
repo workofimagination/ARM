1173ac42bd606aa6f755ee7f06c15865b649cf37
asphergers <carter44354@gmail.com>
2023-05-13 09:37:09 -0400

    fuck it

--- src/utils.rs
+++ src/utils.rs
@@ -52,13 +52,16 @@ impl Utils {
 }
 
 //I stole this shit from stackoverflow
-#[macro_export]
-macro_rules! tuple_into {
-    ($t: expr, ($($ty: ident), *)) => {
-        {
-            let ($($ty,)*) = $t
-            ($($ty as $tym,) *)
+pub mod Util_Macros {
+    macro_rules! tuple_into {
+        ($t: expr, ($($ty: ident), *)) => {
+            {
+                let ($($ty,)*) = $t;
+                ($($ty as $ty,) *)
+            }
         }
     }
+
+    pub(crate) use tuple_into;
 }
 
