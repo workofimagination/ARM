446dac205d59e6f4bc3966691218c2f1a3d93e37
asphergers <carter44354@gmail.com>
2023-06-01 10:02:46 -0400

    the driver can theoretically move in 3D now. this hasn't been tested and I haven't implemented this into the front end

--- src/driver.rs
+++ src/driver.rs
@@ -9,8 +9,10 @@ use crate::utils::{ Point, AngleSet };
 pub struct Driver {
     pub column_motor: Arc<Mutex<TestStepper>>,
     pub beam_motor: Arc<Mutex<TestStepper>>,
+    pub base_motor: Arc<Mutex<TestStepper>>,
     pub column_angle: f32,
     pub beam_angle: f32,
+    pub base_angle: f32,
     pub step_degree: f32,
     pub movement_amount: f32,
     pub micro_delay_default: i64,
@@ -37,18 +39,20 @@ impl Driver {
     pub fn new() -> Driver {
         let column_motor = Arc::new(Mutex::new(TestStepper::new(0, 0)));
         let beam_motor = Arc::new(Mutex::new(TestStepper::new(0, 0)));
+        let base_motor = Arc::new(Mutex::new(TestStepper::new(0, 0)));
         let column_angle = 0.0;
         let beam_angle = 0.0;
+        let base_angle = 0.0;
         let step_degree = 1.0/4.0;
         let movement_amount = 0.01;
-        let micro_delay_default = 1750;
+        let micro_delay_default = 0;
         let micro_delay_min = 1750;
         let micro_delay_max = 4000;
         let current_position = Point { x: 2.0, y: 0.0, z: 0.0 };
         let calc = Calc::new(0.0, 0.0, 1.0);
 
-        return Driver { column_motor, beam_motor, column_angle, beam_angle, step_degree, movement_amount,
-                        micro_delay_default, micro_delay_max, micro_delay_min, current_position, calc 
+        return Driver { column_motor, beam_motor, base_motor, column_angle, beam_angle, base_angle, step_degree,
+                        movement_amount,micro_delay_default, micro_delay_max, micro_delay_min, current_position, calc 
         }
     }
 
@@ -66,19 +70,11 @@ impl Driver {
         if Calc::dist(self.calc.origin.x, self.calc.origin.y, x, y) > self.calc.radius*2.0 { return Err(DriverError::UnReachable) }
 
         let angles = self.calc.get_angles(x, y);
-        let (column_goto, beam_goto) = (angles.column_angle, angles.beam_angle);
 
-        let column_snapped = Calc::snap(Calc::to_degree(column_goto), self.step_degree);
-        let beam_snapped = Calc::snap(Calc::to_degree(beam_goto), self.step_degree);
+        let (beam_steps, column_steps, column_snapped, beam_snapped) = self.get_steps_2d(angles.column_angle, angles.beam_angle);
 
-        let change_in_column = column_snapped - self.column_angle;
-        let change_in_beam = beam_snapped - self.beam_angle - self.column_angle;
-
-        let column_dir = if f32::signum(change_in_column) as i32 == -1 { false } else { true };
-        let beam_dir = if f32::signum(change_in_column) as i32 == -1 { false } else { true };
-
-        let column_steps = (change_in_column/self.step_degree) as i32;
-        let beam_steps = (change_in_beam/self.step_degree) as i32;
+        let column_dir = if i32::signum(beam_steps) == -1 { false } else { true };
+        let beam_dir = if i32::signum(column_steps) == -1 { false } else { true };
 
         let column_thread = Driver::move_motor(&mut self.column_motor, column_steps, column_dir, self.micro_delay_default);
         thread_pool.push(column_thread);
@@ -110,19 +106,11 @@ impl Driver {
         if Calc::dist(self.calc.origin.x, self.calc.origin.y, x, y) > self.calc.radius*2.0 { return Err(DriverError::UnReachable) }
 
         let angles = self.calc.get_angles(x, y);
-        let (column_goto, beam_goto) = (angles.column_angle, angles.beam_angle);
 
-        let column_snapped = Calc::snap(Calc::to_degree(column_goto), self.step_degree);
-        let beam_snapped = Calc::snap(Calc::to_degree(beam_goto), self.step_degree);
+        let (beam_steps, column_steps, column_snapped, beam_snapped) = self.get_steps_2d(angles.column_angle, angles.beam_angle); 
 
-        let change_in_column = column_snapped - self.column_angle;
-        let change_in_beam = beam_snapped - self.beam_angle - self.column_angle;
-
-        let column_dir = if f32::signum(change_in_column) as i32 == -1 { false } else { true };
-        let beam_dir = if f32::signum(change_in_column) as i32 == -1 { false } else { true };
-
-        let column_steps = (change_in_column/self.step_degree) as i32;
-        let beam_steps = (change_in_beam/self.step_degree) as i32;
+        let column_dir = if i32::signum(beam_steps) == -1 { false } else { true };
+        let beam_dir = if i32::signum(column_steps) == -1 { false } else { true };
 
         let column_liner = Driver::get_linear_steps(column_steps);
         let beam_linear = Driver::get_linear_steps(beam_steps);
@@ -163,7 +151,45 @@ impl Driver {
         Ok(())
     }
 
-    pub fn move_direction(&mut self, direction: Direction) -> Result<(), DriverError>{
+    pub fn goto_point_3d(&mut self, x: f32, y: f32, z: f32) -> Result<(), DriverError> {
+        let mut thread_pool: Vec<JoinHandle<()>> = Vec::new();
+
+        let angles = self.calc.get_angles_3d(x, y ,z);
+        let (column_steps, beam_steps, base_steps,
+                column_snapped, beam_snapped, base_snapped) = self.get_steps_3d(angles.column_angle, 
+                                                                                angles.beam_angle, angles.base_angle);
+
+        let column_dir = if i32::signum(beam_steps) == -1 { false } else { true };
+        let beam_dir = if i32::signum(column_steps) == -1 { false } else { true };
+        let base_dir = if i32::signum(base_steps) == -1 { false } else { true };
+
+        let column_thread = Driver::move_motor(&mut self.column_motor, column_steps, column_dir, self.micro_delay_default);
+        thread_pool.push(column_thread);
+            
+        let beam_thread = Driver::move_motor(&mut self.beam_motor, beam_steps, beam_dir, self.micro_delay_default);
+        thread_pool.push(beam_thread);
+
+        let base_thread = Driver::move_motor(&mut self.base_motor, base_steps, base_dir, self.micro_delay_default);
+        thread_pool.push(base_thread);
+
+        for thread in thread_pool {
+            match thread.join() {
+                Ok(()) => (),
+                Err(e) => { println!("{:?}", e); }
+            }
+        }
+
+        self.column_angle = column_snapped;
+        self.beam_angle = beam_snapped;
+        self.base_angle = base_snapped;
+
+        self.current_position.x = x;
+        self.current_position.y = y;
+        self.current_position.z = z;
+        Ok(())
+    }
+
+    pub fn move_direction(&mut self, direction: Direction) -> Result<(), DriverError> {
         match direction {
             Direction::Left => {
                 return self.goto_point(self.current_position.x - self.movement_amount, self.current_position.y);                
@@ -212,6 +238,36 @@ impl Driver {
                 thread::sleep(Duration::from_micros(delay as u64));
             }
         })
+    }
+
+    pub fn get_steps_2d(&self, column_angle: f32, beam_angle: f32) -> (i32, i32, f32, f32) { //this
+        //return type is retarded. too bad
+        let column_snapped = Calc::snap(Calc::to_degree(column_angle), self.step_degree);
+        let beam_snapped = Calc::snap(Calc::to_degree(beam_angle), self.step_degree);
+
+        let change_in_column = column_snapped - self.column_angle;
+        let change_in_beam = beam_snapped - self.beam_angle - self.column_angle;
+
+        let column_steps = (change_in_column/self.step_degree) as i32;
+        let beam_steps = (change_in_beam/self.step_degree) as i32;
+
+        return (column_steps, beam_steps, column_snapped, beam_snapped)
+    }
+
+    //this is also retarded
+    pub fn get_steps_3d(&self, column_angle: f32, beam_angle: f32, base_angle: f32) -> (i32, i32, i32, f32, f32, f32) {
+        let (column_steps, beam_steps, column_snapped, beam_snapped) = self.get_steps_2d(column_angle, beam_angle);
+
+        let base_snapped = Calc::snap(Calc::to_degree(base_angle), self.step_degree);
+        
+        let change_in_base = base_snapped - self.base_angle; 
+
+        let base_steps = (change_in_base/self.step_degree) as i32;
+
+        return (
+                column_steps, beam_steps, base_steps,
+                column_snapped, beam_snapped, change_in_base
+        )
     }
 
     pub fn get_column_position(&self) -> Point {
